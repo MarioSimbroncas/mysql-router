87bba16dc960b64010b7b32d3ed3090781bb69e7
MarioSimbron <simbron420@gmail.com>
2024-01-23 18:10:03 -0600

    Fix - change password

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE prosperwalkadmin;
-CREATE USER 'prospewalkuser'@'%' IDENTIFIED BY 'fh55zqBZTS6OUSwm';
+CREATE USER 'prospewalkuser'@'%' IDENTIFIED BY 'mysql';
 GRANT ALL PRIVILEGES ON prosperwalkadmin.* TO 'prospewalkuser'@'%';
